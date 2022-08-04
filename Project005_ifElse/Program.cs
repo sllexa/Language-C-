a60613b256388e68d3251e6477abe81f5cb0cf40
Aleksei Slivko <sllexa@mail.ru>
2022-08-04 15:52:53 +0300

    Add Project 005 If Else

---
+++ Project005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, " + userName);
+}
